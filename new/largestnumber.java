e19f6983332e0e2001209e6dafa03217f815058d
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-01-23 21:50:17 +0530

    updation

---
+++ new/largestnumber.java
@@ -0,0 +1,31 @@
+// -infinity=in java it write Integer.MIN_VALUE
+// +infinty=in java it writes Integer.MAX_VALUE
+import java.util.*;
+public class largestnumber {
+    public static int greatest(int num[]){
+        int largest=Integer.MIN_VALUE;
+        for(int i=0;i<num.length;i++){
+            if(largest<num[i]);
+            largest=num[i];
+        }
+
+    }
+    public static void main (String[]args){
+        try(Scanner sc=new Scanner(System.in)){
+            System.out.print("Enter the first element:");
+            num[0]=sc.nextInt();
+            System.out.print("Enter the second element:");
+            num[1]=sc.nextInt();
+            System.out.print("Enter the third element:");
+            num[2]=sc.nextInt();
+            System.out.print("Enter the fourth element:");
+            num[3]=sc.nextInt();
+            System.out.print("Enter the fifth element:");
+            num[4]=sc.nextInt();
+            System.out.print("Enter the sixth element:");
+            num[5]=sc.nextInt();
+            int num[]=new int[6];
+            greatest();
+        }
+    }
+}
