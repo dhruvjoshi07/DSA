7bb88bb026906d068d7c455481d1d32f153b33b0
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-01-28 12:14:25 +0530

    28-01-26

--- old/linear_search.java
+++
@@ -1,9 +0,0 @@
-package old;
-public class linear_search {
-    public static linear(int)
-    public static void main(String[] args) {
-        int arr[]={2,4,6,8,10,12,14,16};
-        int key=10;
-
-    }
-}
--- old/myfirst.java
+++
@@ -1,6 +0,0 @@
-package old;
-public  class myfirst{
-    public static void main(String[] args){
-        System.out.print("Hello world");
-    }
-}
