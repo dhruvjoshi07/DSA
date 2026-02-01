c7363a9789226f66a7a28c86295da900a3faea41
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-02-01 23:00:01 +0530

    01-02-2026

--- new/binarysearch.java
+++ new/binarysearch.java
@@ -10,7 +10,9 @@ pseudo code-
        mid>key (left)(end=mid-1)
        mid<key(right)(start=mid+1)
 */
+/* 
 public class binarysearch {
+
     public static int search(int num[],int key){
         int start=0;
         int end=num.length-1;
@@ -31,5 +33,40 @@ public class binarysearch {
         int num[]={2,4,6,8,10,12,14,16,18,20};
         int key=18;
         System.out.println("The index key is on:"+ search(num,key));
+    }
+}
+*/
+public class binarysearch {
+
+    static int search(int arr[], int x) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == x) {
+                return mid;
+            } 
+            else if (arr[mid] < x) {
+                low = mid + 1;
+            } 
+            else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 3, 5, 7, 9, 11};
+        int x = 7;
+
+        int index = search(arr, x);
+
+        if (index == -1)
+            System.out.println("Element not found");
+        else
+            System.out.println("Element found at index: " + index);
     }
 }
