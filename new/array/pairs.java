8b5e14c63502b27498194ee7820504361eeca572
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-02-04 21:21:50 +0530

    04-02-2026

--- new/array/pairs.java
+++ new/array/pairs.java
@@ -1,25 +1,3 @@
-/* 
-package array;
-
-public class pairs {
-    public static void pr(int n[]){
-        int tp=0;
-        for(int i=0;i<n.length;i++){
-            for(int j=i+1;j<n.length;j++){
-                System.out.print("("+n[i]+","+n[j]+")");
-                tp++;
-            }
-            System.out.println();
-        }
-        System.out.println("Total pairs:"+tp);
-    }
-    public static void main(String[] args){
-        int n[]={2,4,6,8,10};
-        pr(n);
-    }  
-}
-*/
-
 //This is a question of reverse Aarray and make the pairs of reveerse array
 package array;
 import java.util.*;
@@ -97,3 +75,24 @@ public class pairs {
         }
     }
 
+/* 
+package array;
+
+public class pairs {
+    public static void pr(int n[]){
+        int tp=0;
+        for(int i=0;i<n.length;i++){
+            for(int j=i+1;j<n.length;j++){
+                System.out.print("("+n[i]+","+n[j]+")");
+                tp++;
+            }
+            System.out.println();
+        }
+        System.out.println("Total pairs:"+tp);
+    }
+    public static void main(String[] args){
+        int n[]={2,4,6,8,10};
+        pr(n);
+    }  
+}
+*/
