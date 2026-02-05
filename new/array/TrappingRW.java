9e3c39e4a0f22e4910ff1e8151da316fcaf03ee5
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-02-05 23:13:43 +0530

    05-02-2026

--- new/array/TrappingRW.java
+++ new/array/TrappingRW.java
@@ -1,5 +1,12 @@
 package array;
+/*Que.Given n non negative intgers representing an elevaion map
+ where the width of each bar is 1,compute how much water 
+ it can trap after  raining*/
+ //elevation=height=[4,2,0,6,3,2,5]
+ //We use "auxillary array"
 import java.util.*;
+//logic=(w-x)*widthofbar:w iswater level and x is height of bar
+//it gives traped water
 public class TrappingRW {
     
 }
