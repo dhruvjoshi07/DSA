30ea504b034e61b379dbee5c760ca7ce82e813d5
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-02-05 23:46:31 +0530

    05-02-2026

---
+++ new/array/impque.java
@@ -0,0 +1,21 @@
+package array;
+/*Que.Given n non negative intgers representing an elevaion map
+ where the width of each bar is 1,compute how much water 
+ it can trap after  raining*/
+ //elevation=height=[4,2,0,6,3,2,5]
+ //We use "auxillary array" means helper
+//logic=trapped water=waterlevel-height[i]:it gives traped water
+public class impque {
+//learning:single bar,double bar,ascending or descending bar=no trap
+
+    public static int trappedRainWaer(int height[]){
+        //cal left max boundry
+        //cal right max boundry
+        //loop
+        //waterlvl=min(leftmax,rightmax)
+        //logic
+    }
+    public static void main(String[] args){
+        int height []={4,2,0,6,3,2,5};
+    }
+}
