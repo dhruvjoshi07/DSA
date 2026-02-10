e22df8fbb4e15052dcab7ad1073218c6cd9904ed
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-02-10 23:22:54 +0530

    10-02-2026

--- myfirst.java
+++ myfirst.java
@@ -1,5 +1,5 @@
 public  class myfirst{
     public static void main(String[] args){
-        System.out.print("Hello world!Myself dhruv joshi");
+        System.out.print("Hello world!Myself dhruv joshi and i m doing java pratice");
     }
 }
