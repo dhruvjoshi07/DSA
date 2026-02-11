883eb212b7f16f6665f555c7d1574052a94446f6
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-02-11 22:43:58 +0530

    11-02-2026

--- old/loops.java
+++ old/loops.java
@@ -142,3 +142,41 @@ public class loops{
         }
     }
 }
+
+
+/*import java.util.*;
+public class learning2{
+    public static void main(String[] args) {
+        try(Scanner sc =new Scanner(System.in)){
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println(sum);
+        sc.close();
+        }
+    }
+}*/
+/*import java.util.*;
+public class learning2{
+    public static void main(string[] args){
+        try(Scanner sc=new Scanner(System.in)){
+            float r1=sc.nextFloat();
+            float radius =3.14f*r1*r1;
+            System.out.println("AREA OF CIRCLE:");
+            System.out.println(radius);
+            sc.close();
+        }
+    }
+}*/
+/* 
+public class learning2{
+    public static void main(String[] args) {
+        char a='a';
+        char b='b';
+        System.out.println((int)(b));
+        System.out.println((int)(a));
+        System.out.println(b-a);//it gives 98-97=1
+        // b-a 'char,byte,short' changes to 'int' tbhi ho skte jb expression ho
+    }
+}
+    */
