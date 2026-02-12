8d116445acbe525f9c54661ad37ad3bd7e823507
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-02-12 22:28:06 +0530

    Updated bubblesort.java

--- new/sorting/bubblesort.java
+++ new/sorting/bubblesort.java
@@ -1,5 +1,4 @@
 package sorting;
-import java.util.*;
 public class bubblesort {
     public static void sort(int arr[]){
         for(int turn=0;turn<arr.length-1;turn++){
@@ -25,15 +24,15 @@ logic:
 for(i=0 to n-2)
     for(j=0 to n-2-i)
 */
-    public static void main(String [] args){
-        try(Scanner sc=new Scanner(System.in)){
-            int arr[]=new int[50];
-            int size=5;
-            for(int i=0;i<size;i++){
-                arr[i]=sc.nextInt();
-            }
-            
-
+    public static void printarr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
         }
+        System.out.println();
+    }
+    public static void main(String [] args){
+            int arr[]={5,4,1,3,2};
+            sort(arr);
+            printarr(arr);
     }
 }
