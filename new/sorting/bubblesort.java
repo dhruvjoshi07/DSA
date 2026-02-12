eeb8cde699ab83d3815fc537f6b1d7b01977f35e
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-02-12 22:04:53 +0530

    12-02-2026

--- new/sorting/bubblesort.java
+++ new/sorting/bubblesort.java
@@ -1,11 +1,18 @@
 package sorting;
+import java.util.*;
 public class bubblesort {
     public static void sort(int n){
 
     }
     public static void main(String [] args){
         try(Scanner sc=new Scanner(System.in)){
+            int arr[]=new int[50];
+            int size=5;
+            for(int i=0;i<size;i++){
+                arr[i]=sc.nextInt();
+            }
             
+
         }
     }
 }
