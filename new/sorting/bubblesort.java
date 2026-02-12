3761a9abc3ebeeb185116094019eb82867fb9ec3
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-02-12 22:10:46 +0530

    Updated bubblesort.java

--- new/sorting/bubblesort.java
+++ new/sorting/bubblesort.java
@@ -4,6 +4,18 @@ public class bubblesort {
     public static void sort(int n){
 
     }
+    /*Bubble sort=5,4,1,3,2
+ 5 ko her kisi se compare kra and we got
+ 4 1 3 2 5
+ now 4 se compare
+ 1 3 2 4 5
+ now 3 ko compare
+ 1 2 3 4 5
+ we see now arr is sorted
+logic:
+for(i=0 to n-2)
+    for(j=0 to n-2-i)
+*/
     public static void main(String [] args){
         try(Scanner sc=new Scanner(System.in)){
             int arr[]=new int[50];
