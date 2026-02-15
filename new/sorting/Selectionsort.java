43d160426f2eecf82bb9ea5b41a332b6217fa843
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-02-15 21:45:22 +0530

    15-02-26

---
+++ new/sorting/Selectionsort.java
@@ -0,0 +1,16 @@
+package sorting;
+import java.util.*;
+public class Selectionsort {
+    public void sort(int arr[]){
+
+    }
+    public static void main(String args[]){
+        try(Scanner sc= new Scanner(System.in)){
+            int arr[]=new int[5];
+            for(int i=0;i<arr.length;i++){
+                arr[i]=sc.nextInt();
+            }
+
+        }
+    }
+}
