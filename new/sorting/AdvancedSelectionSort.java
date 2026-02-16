47112996e8e8767fdd26f331e26a1b785bebfec1
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-02-16 23:26:55 +0530

    16-02-26

---
+++ new/sorting/AdvancedSelectionSort.java
@@ -0,0 +1,55 @@
+package sorting;
+public class AdvancedSelectionSort {
+
+    public static void selectionSortMinMax(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            int minIndex = left;
+            int maxIndex = left;
+
+            // Find minimum and maximum in one loop
+            for (int i = left; i <= right; i++) {
+                if (arr[i] < arr[minIndex]) {
+                    minIndex = i;
+                }
+                if (arr[i] > arr[maxIndex]) {
+                    maxIndex = i;
+                }
+            }
+
+            // Swap minimum with left
+            int temp = arr[left];
+            arr[left] = arr[minIndex];
+            arr[minIndex] = temp;
+
+            // If max element was at left, update its index
+            if (maxIndex == left) {
+                maxIndex = minIndex;
+            }
+
+            // Swap maximum with right
+            temp = arr[right];
+            arr[right] = arr[maxIndex];
+            arr[maxIndex] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {64, 25, 12, 22, 11, 90, 5};
+
+        selectionSortMinMax(arr);
+
+        System.out.println("Sorted Array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
