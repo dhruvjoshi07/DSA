ce0deeb5a00d2f8accaac48bb503a3edd026eb50
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-02-16 23:28:52 +0530

    16-02-26

--- new/sorting/AdvancedSelectionSort.java
+++ new/sorting/AdvancedSelectionSort.java
@@ -53,3 +53,56 @@ public class AdvancedSelectionSort {
     }
 }
 
+public class ProductPriceSort {
+
+    public static void advancedSelectionSort(int[] arr) {
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            int minIndex = left;
+            int maxIndex = left;
+
+            for (int i = left; i <= right; i++) {
+                if (arr[i] < arr[minIndex]) {
+                    minIndex = i;
+                }
+                if (arr[i] > arr[maxIndex]) {
+                    maxIndex = i;
+                }
+            }
+
+            // Swap minimum to left
+            int temp = arr[left];
+            arr[left] = arr[minIndex];
+            arr[minIndex] = temp;
+
+            if (maxIndex == left) {
+                maxIndex = minIndex;
+            }
+
+            // Swap maximum to right
+            temp = arr[right];
+            arr[right] = arr[maxIndex];
+            arr[maxIndex] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] prices = {4999, 1999, 9999, 2999, 1499, 7999};
+
+        advancedSelectionSort(prices);
+
+        System.out.println("Sorted Prices:");
+        for (int price : prices) {
+            System.out.print(price + " ");
+        }
+    }
+}
+
