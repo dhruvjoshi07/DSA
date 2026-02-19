8e2889eac6f0bfc4e0e9b4643415cd6816c2feac
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-02-19 18:39:46 +0530

    19-02-26

--- new/sorting/Insertionsort.java
+++ new/sorting/Insertionsort.java
@@ -1,5 +1,13 @@
 package sorting;
-
+import java.util.*;
 public class Insertionsort {
-    
+    public static void main(String[] args){
+        try(Scanner sc=new Scanner(System.in)){
+            int arr[]=new int[50];
+            int size=8;
+            for(int i=0;i<size;i++){
+                arr[i]=sc.nextInt();
+            }
+        }
+    }
 }
