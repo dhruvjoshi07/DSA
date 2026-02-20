9e6771b371f0053148e1c9e5376b0a07ac840bc3
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-02-20 19:58:42 +0530

    20-02-26

--- new/sorting/effective.java
+++ new/sorting/effective.java
@@ -1,22 +1,41 @@
 package sorting;
 import java.util.*;
 public class effective {
-    public static void printarr(int arr[],int size){
+
+    public static void revarr(Integer arr[],int size){
+        /*yea isliye bnaya kyuki collection jo hai object pr kaam krta
+        and kyuki int ek primitive data type hai toh array error dega
+        so ham 'Integer' use krenge taki Collection object pr kaam kre
+        */
+        Arrays.sort(arr,0,size,Collections.reverseOrder());
+    }
+
+
+    public static void printarr(Integer arr[],int size){
         for(int i=0;i<size;i++){
             System.out.println(arr[i]+" ");
         }
     }
+
     public static void main(String [] args){
         try(Scanner sc=new Scanner(System.in)){
-            int arr[]=new int[50];
+            Integer arr[]=new Integer[50];
             int size=5;
 
             System.out.println("Enter the elements for sorting:");
             for(int i=0;i<size;i++){
                 arr[i]=sc.nextInt();
             }
-            System.out.print("The sorted array is: ");
-            Arrays.sort(arr,0,size);
+
+            //Ascending sort
+            Arrays.sort(arr,0,size); //line of code
+            System.out.println("Ascending sorted array is: ");
+            printarr(arr,size);
+
+            //Descending sort
+            // Integer mrevarr[]=new Integer[50];
+            revarr(arr,size);
+            System.out.println("Descending sorted array is:");
             printarr(arr,size);
 
             sc.close();
