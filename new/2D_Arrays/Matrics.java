564d9902b6dacfa714fd300f759f2acef971e23b
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-02-24 23:04:46 +0530

    24-02-26

--- new/2D_Arrays/Matrics.java
+++ new/2D_Arrays/Matrics.java
@@ -1,5 +1,30 @@
+import java.util.*;
 public class Matrics {
+
+    //search an cell in matrix
+    // public static boolean search(int matrix[][],int key){
+
+    // }
     public static void main(String[] args) {
-        
+        try(Scanner sc=new Scanner(System.in)){
+            int matrix[][]=new int[3][3];
+            int n=matrix.length, m =matrix[0].length;
+
+            //Declare rows and columns
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    matrix[i][j]=sc.nextInt();
+                }
+            }
+            
+            //Output
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    System.out.print(matrix[i][j]+" ");
+                }
+                System.out.println();
+            }
+            sc.close();
+        }
     }
 }
