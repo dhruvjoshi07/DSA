eeb6a820be0cb201980e0aa6a1ad197a0c9c0cf8
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-02-25 22:56:46 +0530

    25-02-26

--- new/2D_Arrays/SpiralMatrix.java
+++ new/2D_Arrays/SpiralMatrix.java
@@ -7,8 +7,16 @@ public class SpiralMatrix {
                 for(int j=0;j<matrix[0].length;j++){
                     matrix[i][j]=sc.nextInt();
                 }
-                
+
             }
+            /* 
+            //approach
+            start row=0 then 1 in iteration
+            end row=n-1 then 2 in iteration
+            start column=0 then 2 in iteration1 
+            end column=m-1 then 2 in iteration
+            */
+           
             sc.close();
         }
     }
