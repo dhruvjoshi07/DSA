079c74f483fa5b847efd861aa5c920502032b1f4
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-02-26 22:41:07 +0530

    26-02-26

--- new/2D_Arrays/SpiralMatrix.java
+++ new/2D_Arrays/SpiralMatrix.java
@@ -1,5 +1,24 @@
 import java.util.*;
 public class SpiralMatrix {
+    public static void arr2d(int matrix[][]){
+         int startRow=0;
+                int startCol=0;
+                int endRow=matrix.length-1;
+                int endCol=matrix[0].length-1;
+                while(startRow<=endRow && startCol<=endCol){
+                    
+                //top
+                for(int j=startCol;j<=endCol;j++){
+                    System.out.print(matrix[startRow][j]+" ");
+                }
+                
+                //right
+
+                //bottom
+
+            }
+        }
+    }
     public static void main(String[] args){
         try(Scanner sc= new Scanner(System.in)){
             int matrix[][]=new int[4][4];
@@ -7,8 +26,8 @@ public class SpiralMatrix {
                 for(int j=0;j<matrix[0].length;j++){
                     matrix[i][j]=sc.nextInt();
                 }
-
-            }
+            arr2d(matrix);
+            sc.close();
             /* 
             //approach
             start row=0 then 1 in iteration
@@ -16,8 +35,6 @@ public class SpiralMatrix {
             start column=0 then 2 in iteration1 
             end column=m-1 then 2 in iteration
             */
-           
-            sc.close();
         }
     }
 }
