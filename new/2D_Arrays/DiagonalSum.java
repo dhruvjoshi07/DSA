d4223d684580e2d920ade66da02a714b557b2db7
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-02-28 23:23:38 +0530

    28-02-26

--- new/2D_Arrays/DiagonalSum.java
+++ new/2D_Arrays/DiagonalSum.java
@@ -14,5 +14,13 @@ public class DiagonalSum {
                 matrix[i][j]=sc.nextInt();
             }
         }
+        
+        //output
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
