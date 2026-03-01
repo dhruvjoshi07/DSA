ed3c8b9402768e91c641c54dbf75d366091f6afe
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-03-01 23:23:15 +0530

    01-03-26

--- new/2D_Arrays/DiagonalSum.java
+++ new/2D_Arrays/DiagonalSum.java
@@ -2,16 +2,16 @@ import java.util.*;
 public class DiagonalSum {
     public static int diagonal(int matrix[][]){
         int sum=0;
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(i==j){
-                    sum+=matrix[i][j];
-                }
-                else if(i+j==matrix.length-1){
-                    sum+=matrix[i][j];
-                }
-            }
-        }
+        // for(int i=0;i<matrix.length;i++){
+        //     for(int j=0;j<matrix[0].length;j++){
+        //         if(i==j){
+        //             sum+=matrix[i][j];
+        //         }
+        //         else if(i+j==matrix.length-1){
+        //             sum+=matrix[i][j];
+        //         }
+        //     }
+        // }
         return sum;
     }
     public static void main(String [] args){
