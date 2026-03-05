d5a5db9c4030b057a5b0ddcad104737b0c9ccffa
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-03-05 23:40:28 +0530

    updation

--- new/2D_Arrays/search.java
+++ new/2D_Arrays/search.java
@@ -1,5 +1,17 @@
 public class search {
+    /* Logic:
+    (0,m-1)
+        key<cell value
+            LEFT
+        key>cell value
+            Bottom
+            */
+    
     public static void main(String args[]){
-        //
+        int matrix[][]={{10,20,30,40},
+                        {15,25,35,45},
+                        {27,29,37,48},
+                        {32,33,39,50}};
+        int key=33;
     }
 }
