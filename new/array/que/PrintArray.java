3511bdf11143ad849f343e87b1b08c486aecdbef
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-03-10 22:49:46 +0530

    first commit

--- new/array/que/PrintArray.java
+++ new/array/que/PrintArray.java
@@ -1,3 +1,4 @@
+/* 
 // Shopping price java
 package que;
 
@@ -28,4 +29,15 @@ public class sp {
     }
 }
 
+}
+*/
+public class PrintArray {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        System.out.println("Array elements are:");
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
 }
