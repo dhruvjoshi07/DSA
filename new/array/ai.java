72258ba9820e5d0ff89860cdb2a5cc7bec3e69f1
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-03-18 23:55:14 +0530

    commit

---
+++ new/array/ai.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        // Handle cases where k is larger than array length
+        k %= n; 
+        
+        // 1. Reverse the whole thing: [5,4,3,2,1]
+        reverse(nums, 0, n - 1);
+        // 2. Reverse the first k: [4,5,3,2,1]
+        reverse(nums, 0, k - 1);
+        // 3. Reverse the rest: [4,5,1,2,3]
+        reverse(nums, k, n - 1);
+    }
