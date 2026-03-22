03211a9a2c461980c7cbd6e141407d5ee5b8cf7a
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-03-22 21:51:15 +0530

    commit

--- myfirst.java
+++ myfirst.java
@@ -1,7 +1,9 @@
 public  class myfirst{
     public static void main(String[] args){
-        System.out.print("Hello world!Myself dhruv joshi");
+        System.out.print("Hello world!");
+        System.out.print("Myself dhruv joshi");
         
         System.out.println("i m doing java pratice");
+        
     }
 }
