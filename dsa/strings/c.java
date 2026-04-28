7693ae486d5e8b8aecd3aaaf4da09d6dda444e0d
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-04-28 19:31:45 +0530

    strings

---
+++ dsa/strings/c.java
@@ -0,0 +1,19 @@
+package strings;
+//IMP:Check if a string is a Palindrome like, "racecar","noon","madam"
+import java.util.*;
+public class c {
+
+    public static void palindrome(String word){
+        
+    }
+    public static void main(String args[]){
+        try(Scanner sc= new Scanner(System.in)){
+
+            System.out.println("Check if a string is a Palindrome...");
+            String word;
+            word=sc.nextLine();
+            System.out.println(word);
+            sc.close();
+        }
+    }
+}
