a0158caa28edd2bd90749cade0188b1a16afacc8
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-04-28 17:08:12 +0530

    New chapter

---
+++ dsa/strings/a.java
@@ -0,0 +1,35 @@
+package strings;
+// import java.util.*;
+public class a {
+    public static void main(String args[]){
+        /* 
+        try(Scanner sc= new Scanner(System.in)){
+        String name;
+        // name=sc.next();   //yea bs first word leta like dhruv joshi ka bs dhruv
+        name=sc.nextLine();
+        System.out.println(name);
+
+        */
+
+       /* 
+        //Remember:Ham string.length() yea length fx hai but array mei property
+        String fullname="Dhruv Joshi";
+        System.out.println(fullname.length());
+        */
+
+        /*
+        //kese kese defined kr skte
+        char arr[]={'a','b','c','d'};
+        String str="abcd";
+        String str2= new String("xyz");
+        //NOTE:  Strings are IMMUTABLE
+        */
+
+        //concatenation
+        String firstName="Muskan";
+        String lastName="Dixit";
+        String fullName=firstName+" "+lastName;
+        System.out.println(fullName+"is my name");
+        }
+    }
+// }
