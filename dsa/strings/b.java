00b096cab33b67dada594d45741fd3224ed57396
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-04-28 19:19:35 +0530

    strings

--- dsa/strings/b.java
+++ dsa/strings/b.java
@@ -1,5 +1,21 @@
 package strings;
-
+import java.util.*;
+//ek ek chr ko dhundna k liye
 public class b {
-    
+    public static void printLetters(String str){
+        for(int i=0;i<str.length();i++){
+            System.out.println(str.charAt(i)+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        try(Scanner sc= new Scanner(System.in)){
+            System.out.println("Enter the name: ");
+            String name;
+            name=sc.nextLine();
+            System.out.println(name);
+            printLetters(name);
+            sc.close();
+        }
+    }
 }
