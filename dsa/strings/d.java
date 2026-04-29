fe7f9966bebf679235000a6c727e6b45c54aed2f
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-04-29 09:14:43 +0530

    strings

---
+++ dsa/strings/d.java
@@ -0,0 +1,14 @@
+package strings;
+//comparison 
+public class d {
+    public static boolean stringComparison(String str1,String str2){
+        //yaha ham == nhi lgate string mei
+        while(str1.equals(str2)){
+            return true;
+        }
+        return false;
+    }
+    public static void main(String args[]){
+
+    }
+}
