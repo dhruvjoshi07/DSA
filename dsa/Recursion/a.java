5d4e1dad1d08b9adf504d1b6a00e4c1419fb1f03
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-04-30 14:16:18 +0530

    strings

---
+++ dsa/Recursion/a.java
@@ -0,0 +1,11 @@
+package Recursion;
+//it is the method of solving a computational problem where the soln depends on soln to small instance of the same problem
+//chote kaam pehle kar lo ,jo hamme pta hota usko ham BASE CLASS nad jisko call krte usko RECURSIVE FUNCTIONS
+//Two Direction=top to down and down to top
+public class a {
+    //Problem1:Print numbers from n to 1 (Decreasing order)
+    //logic:base case define,kaam kya krna hai pta ho ,inner fx ko call lagana
+    public static void main(String args[]){
+         
+    }
+}
