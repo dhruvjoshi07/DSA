82b4a3b4b2c22bc42433eeeab46327f9fadaedee
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-04-30 19:13:09 +0530

    recursion

--- dsa/Recursion/a.java
+++ dsa/Recursion/a.java
@@ -3,7 +3,7 @@ package Recursion;
 //chote kaam pehle kar lo ,jo hamme pta hota usko ham BASE CLASS nad jisko call krte usko RECURSIVE FUNCTIONS
 //Two Direction=top to down and down to top
 public class a {
-    //problem 1 :print the reverse number
+    //Problem1:Print numbers from n to 1 (Decreasing order)
     public static void printDec(int n){
         if(n==1){
         System.out.print(n);
@@ -29,16 +29,40 @@ public class a {
         int x=m*fact(m-1);
         return x;
     }
-    //Problem1:Print numbers from n to 1 (Decreasing order)
+    //problem 4:print sum of first n natural numbers
+    public static int natNum(int n){
+        if(n==1){
+            return 1;
+        }
+        int sum=n+natNum(n-1);
+        return sum;
+    }
+    //problem 5:print nth fibonacci number
+    public static int fib(int x){
+        if(x==0 || x==1){
+            return x;
+        }
+        return fib(x-1)+fib(x-2);
+    }
+    //problem 6:check if a given array is sorted or not
+    public static void arrSort(int arr[]){
+        
+    }
     //logic:base case define,kaam kya krna hai pta ho ,inner fx ko call lagana
     public static void main(String args[]){
-         int n=10;
+         int n=5;
          int m=5;
+         int x=10;
          printDec(n);
+
          System.out.println();
          printInc(n);
+
          System.out.println();
          System.out.println(fact(m));
+
+         System.out.println(natNum(n));
+         System.out.println(fib(x));
          //call Stack while recursion 
          //Stack overflow when base is not there
 
