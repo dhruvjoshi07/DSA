5a93690094cfd3ca4fbb95aa0e42f94334e51d00
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-05-03 19:18:25 +0530

    recursion

---
+++ dsa/Recursion/b.java
@@ -0,0 +1,8 @@
+package Recursion;
+//Problem 7:WAF to find first occurance of an element in array
+public class b {
+    public static void main(String args[]){
+        int arr[]={4,6,1,8,2,8};
+        
+    }
+}
