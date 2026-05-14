45062d7f8d38b4c36d4b21ec543b08670a1cda2b
Dhruv Joshi <1002dhruvJ@gmail.com>
2026-05-14 22:24:16 +0530

    Change print to println for Hello world message

--- myfirst.java
+++ myfirst.java
@@ -1,6 +1,6 @@
 public  class myfirst{
     public static void main(String[] args){
-        System.out.print("Hello world!");
+        System.out.println("Hello world!");
         System.out.println("Myself dhruv joshi");
         
         System.out.println("i m doing java pratice");
