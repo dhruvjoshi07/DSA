20f61df5b325640a65bbab1337df58dac9a96d14
Dhruv Joshi <1002dhruvJ@gmail.com>
2026-06-20 20:53:11 +0530

    Update myfirst.java

--- myfirst.java
+++ myfirst.java
@@ -1,7 +1,7 @@
 public  class myfirst{
     public static void main(String[] args){
         System.out.println("Hello world!");
-        System.out.println("Myself dhruv joshi");
+        System.out.println("yoyo,Myself dhruv joshi");
         
         System.out.println("i m doing java pratice");
         
