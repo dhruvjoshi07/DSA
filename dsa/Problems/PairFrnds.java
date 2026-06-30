93b2f4dc63cb6b14044646f4e6647696ce88e3b0
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-06-30 20:43:36 +0530

    recursion problems

--- dsa/Problems/PairFrnds.java
+++ dsa/Problems/PairFrnds.java
@@ -5,7 +5,21 @@ Each friend can be paired only once.Find out the total number of ways in which f
 can remain single or can be paired up.
 */
 public class PairFrnds {
+    public static int frndpairing(int n){
+        //base case
+        if(n==1 || n==2){
+            return n;
+        }
+        //pairs+choices+totalways
+        return frndpairing(n-1)+(n-1)*frndpairing(n-2);
+    }
     public static void main(String[]args){
-        
+   /*
+   logic:
+   n-1=pair choices
+   n-2=1 pair ways
+   total ways=n-1+n-1*n-2
+    */  
+    frndpairing(6);
     }
 }
