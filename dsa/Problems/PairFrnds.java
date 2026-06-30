1acfe15f29948d72e80416ac22646e7380628c11
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-06-30 20:18:00 +0530

    recursion problems

---
+++ dsa/Problems/PairFrnds.java
@@ -0,0 +1,11 @@
+package Problems;
+/*FRIENDS PAIRING PROBLEM
+Given n friends ,each one can remain single or can be paired up with some other friend.
+Each friend can be paired only once.Find out the total number of ways in which friends 
+can remain single or can be paired up.
+*/
+public class PairFrnds {
+    public static void main(String[]args){
+        
+    }
+}
---
+++ dsa/Problems/StrDuplicate.java
@@ -0,0 +1,24 @@
+package Problems;
+// conditions ese hai bs a-z aa skte na ki A,@ ,etc.isliye liye hammei hash usena padega
+// index find=currchar-'a'
+public class StrDuplicate {
+    public static void removeduplicates(String str,Stringbuilder newStr,int index,boolean arr[]){
+        //base case
+        if(index == str.length()){
+            System.out.println(newStr);
+            return;
+        }
+        //work
+        char currChar=str.charAt(index);
+        if(arr[currChar-'a']==true){
+            removeduplicates(str,newStr,index+1,arr);
+        }else{
+            arr[currChar-'a']=true;
+            removeduplicates(str,newStr.append(currChar),index+1,arr);
+        }
+    }
+    public static void main(String[] args){
+        String str="appnacollege";
+        removeduplicates(str,new StringBuilder(""),0,new boolean[26]);
+    }
+}
