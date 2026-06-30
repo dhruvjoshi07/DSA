a9470ca7f9f36ff29377f8752d8dd58a291334a0
dhruvjoshi07 <1002dhruvj@gmail.com>
2026-06-30 21:06:37 +0530

    recursion problems

---
+++ dsa/Problems/binaryStr.java
@@ -0,0 +1,17 @@
+package Problems;
+/*Binary Strings Problem
+Print all binary strings of size N without consecutive ones
+ example,binary strings made up of "01010","0000" but in que
+ hammei esi string print krni jismei 0 and 1 lagatr ni aye.for example
+ 010011=not valid cuz end mei 11 aa gya
+ 010101=valid 
+ */
+public class binaryStr {
+    public static void binstr(int n,int lastplace,String str){
+    //base class
+
+    }
+    public static void main(String[] args){
+
+    }
+}
